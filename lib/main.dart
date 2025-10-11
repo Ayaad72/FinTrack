91ba8e4462b1962abbcc28adfa602da61d11d57f
Ayaad72 <ayaadalavi@gmail.com>
2025-10-11 21:52:06 +0500

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ void main() async {
     final loggedIn = await LocalStorage.isLoggedIn();
     runApp(SpendWiseApp(startOnDashboard: loggedIn == true));
   } catch (e) {
-    // If czxczxczx's any error, start with login screen
+    // If dasdasfsddf  werfwerwer's any error, start with login screen
     runApp(const SpendWiseApp(startOnDashboard: false));
   }
 }
