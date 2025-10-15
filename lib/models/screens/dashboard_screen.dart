0f2905a574ef0eca905e45c3b47a5884f2d35375
Ayaad72 <ayaadalavi@gmail.com>
2025-10-15 22:27:05 +0500

    changes

--- lib/models/screens/dashboard_screen.dart
+++ lib/models/screens/dashboard_screen.dart
@@ -9,7 +9,7 @@ class DashboardScreen extends StatefulWidget {
   State<DashboardScreen> createState() => _DashboardScreenState();
 }
 
-// Hey i am a dsaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa?
+// Hey i am a dasssssssssssssssssssssss?
 class _DashboardScreenState extends State<DashboardScreen> {
   final List<Map<String, dynamic>> _transactions = [
     {
