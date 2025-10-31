a95c48b23ba8b96b5e0832eb45258dd7546e596c
Ayaad72 <ayaadalavi@gmail.com>
2025-10-31 18:55:50 +0500

    Modified the Theme

--- lib/models/screens/dashboard_screen.dart
+++ lib/models/screens/dashboard_screen.dart
@@ -9,7 +9,7 @@ class DashboardScreen extends StatefulWidget {
   State<DashboardScreen> createState() => _DashboardScreenState();
 }
 
-// hey i ccc a Comment
+// hey i dassssssssssssssssss a Comment
 class _DashboardScreenState extends State<DashboardScreen> {
   final List<Map<String, dynamic>> _transactions = [
     {
