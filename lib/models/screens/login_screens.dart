0cbc76f7ae969cd7dbabd34e0311c82591792a32
Ayaad72 <ayaadalavi@gmail.com>
2025-11-02 17:28:24 +0500

    changes

--- lib/models/screens/login_screens.dart
+++ lib/models/screens/login_screens.dart
@@ -13,7 +13,7 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   final emailCtrl = TextEditingController();
   final passCtrl = TextEditingController();
-  // Hello wdfgggggdddddddddddddddddddddddddggggggggorfffffffffffffgggffffffffffggggld my name is Akshay
+  // Hello wdfgggggddddddddddddddddddddddddddddddddddggggggggorfffffffffffffgggffffffffffggggld my name is Akshay
   void _login() async {
     final success = await LocalStorage.login(emailCtrl.text, passCtrl.text);
     if (success) {
