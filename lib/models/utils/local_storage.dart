b77ee666d95dd6b8b30177fd321815ee42c38e66
Ayaad72 <ayaadalavi@gmail.com>
2025-11-21 23:39:36 +0500

    changes

--- lib/models/utils/local_storage.dart
+++ lib/models/utils/local_storage.dart
@@ -13,7 +13,7 @@ class LocalStorage {
     await prefs.setBool('loggedIn', true);
   }
 
-  // Hey ewwwwwwwwwwwwwwwwwwwwwww
+  // Hey Commentawesadasdas
   static Future<bool> login(String email, String password) async {
     final prefs = await SharedPreferences.getInstance();
     final savedEmail = prefs.getString('email');
