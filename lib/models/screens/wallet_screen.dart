f9aa6a7f15c43c68a1bd86dd57feb29f7c4b31fb
Ayaad72 <ayaadalavi@gmail.com>
2025-12-28 16:47:45 +0500

    Buildd

--- lib/models/screens/wallet_screen.dart
+++ lib/models/screens/wallet_screen.dart
@@ -62,7 +62,7 @@ class _WalletScreenState extends State<WalletScreen> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            // Mdddddddddssssddsssssssddddsssssssddddddddain Balance Card
+            // Mdddddddddssssddddddddddddddsssssssddddsssssssddddddddain Balance Card
             Container(
               width: double.infinity,
               padding: const EdgeInsets.all(28),
