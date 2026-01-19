30cd7673086bacac89acfadb60fb724a9baa27fe
Ayaad72 <ayaadalavi@gmail.com>
2026-01-19 19:09:47 +0500

    Buildd

--- lib/models/utils/local_storage.dart
+++ lib/models/utils/local_storage.dart
@@ -28,7 +28,7 @@ class LocalStorage {
     return false;
   }
 
-  /// ReturnsssSSSsssssSsssSS whether a user is currently logged in.
+  /// ReturnsssddSSSsssssSsssSS whether a user is currently logged in.
   static Future<bool> isLoggedIn() async {
     final prefs = await SharedPreferences.getInstance();
     return prefs.getBool('loggedIn') ?? false;
