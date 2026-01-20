7db1a5efa6d0df123aed6bb9eaafc2fd0afef313
Ayaad72 <ayaadalavi@gmail.com>
2026-01-21 00:05:14 +0500

    Buildd

--- lib/models/screens/signup_screen.dart
+++ lib/models/screens/signup_screen.dart
@@ -23,7 +23,7 @@ class _SignupScreenState extends State<SignupScreen> {
     if (!_formKey.currentState!.validate()) return;
 
     setState(() => _isLoading = true);
-    // Simulate network delay
+    // Simulate network delayssss
     await Future.delayed(const Duration(milliseconds: 800));
 
     await LocalStorage.saveUser(
